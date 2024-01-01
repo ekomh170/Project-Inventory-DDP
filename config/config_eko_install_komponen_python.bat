a7b84aa8113b2368252ad50f58eae8a2ca9e5f8c
Eko Muchamad Haryono <ekomh13@gmail.com>
2024-01-01 17:08:01 +0700

    Menambahkan konfigurasi python pada aplikasi

---
+++ config/config_eko_install_komponen_python.bat
@@ -0,0 +1,8 @@
+:: Menjalankan perintah instalasi Tkinter dan Pillow
+pip install tk
+pip install pillow
+pip install ttkbootstrap
+pip install ttkthemes
+
+:: Menjalankan perintah untuk  membuat program menjadi file .exe
+pip install pyinstaller

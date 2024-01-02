0ba8aca573adf56ba5af5a0b9c4e9322ee908922
Eko Muchamad Haryono <ekomh13@gmail.com>
2024-01-02 23:57:46 +0700

    Melakukan Init pada file utama, untuk file run program tinker

---
+++ app_stock_management_main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from app_stock_management_gui_modul import StockManagementGUI
+
+# Main program
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = StockManagementGUI(root)
+    root.mainloop()
